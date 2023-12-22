cfc8514943e7cec0dba0b07ff8990bf85c07c27d
safakun <safarovdeem@gmail.com>
2023-12-22 07:35:03 +0500

    aded mongoose connectoptions

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
  
 export default function connectDB() {
   const url = process.env.MONGO_URI;
  
   try {
     mongoose.connect(String(url), {
-      //  useNewUrlParser: true, 
-     //   useUnifiedTopology: true, 
-     //   useCreateIndex: true
-    });
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useCreateIndex: true
+    } as ConnectOptions);
   } catch (err: any) {
     console.error(err.message);
     process.exit(1);
